2fc0b4268e1124164dbfc94c2ee316d252d8d5a5
yoccy <t0507yoccy@icloud.com>
2019-06-29 12:49:10 +0900

    ファイルのリンク

--- run.py
+++ run.py
@@ -7,5 +7,9 @@ def index():
     name = "yoccy"
     return render_template('/index.html', name=name)
 
+@app.route('/portfolio.html')
+def portfolio():
+    return render_template('/portfolio.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
