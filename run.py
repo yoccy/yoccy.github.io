8c58b66c81cd5ae717f2c8dcda4126aef5dd1dcf
yoccy <t0507yoccy@icloud.com>
2019-07-01 19:13:16 +0900

    タブ追加

--- run.py
+++ run.py
@@ -7,9 +7,17 @@ def index():
     name = "yoccy"
     return render_template('/index.html', name=name)
 
+@app.route('/skill.html')
+def skill():
+    return render_template('/skill.html')
+
 @app.route('/portfolio.html')
 def portfolio():
     return render_template('/portfolio.html')
+
+@app.route('/interested.html')
+def interested():
+    return render_template('/interested.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
